b64eb4b128dba97f241f9e8086ab61dc73cf41bc
Huptam <moisespayares6@gmail.com>
2022-07-27 21:08:41 -0500

    add product added

--- resources/views/producto/agregar.blade.php
+++ resources/views/producto/agregar.blade.php
@@ -7,53 +7,63 @@
         <div class="row">
             <div class="card shadow-lg border-0 rounded-lg mt-5">
                 <div class="card-body">
-                    <form>
+                    <form action="{{ route('storeProducto') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input class="form-control" id="nombre" type="text"
-                                        placeholder="Digite el nombre del producto" required/>
+                                    <input class="form-control" id="name" name="name" type="text"
+                                        placeholder="Digite el nombre del producto" value="{{old('name')}}"/>
                                     <label for="nombre">Nombre</label>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating mb-3 mb-md-0">
-                                    <input class="form-control" id="distribuidor" type="text"
-                                        placeholder="Digite el nombre del distribuidor" required />
-                                    <label for="distribuidor">Distribuidor</label>
+                                    <input class="form-control" id="description" name="description" type="text"
+                                        placeholder="Digite una descripcion" value="{{old('description')}}"/>
+                                    <label for="descripcion">Descripción</label>
                                 </div>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <div class="form-floating">
-                                    <input class="form-control" id="ciudad" type="text"
-                                        placeholder="Digite la ciudad de origen" />
-                                    <label for="ciudad">Ciudad</label>
+                                    <input class="form-control" id="image" name="image" type="file"
+                                        placeholder="Adjunte la imagen al producto""/>
+                                    <label for="imagen">Adjuntar Imagen</label>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating mb-3 mb-md-0">
-                                    <input class="form-control" id="cantidad" type="number"
-                                        placeholder="Digite la cantidad" required />
-                                    <label for="cantidad">Cantidad</label>
+                                    <input class="form-control" id="barcode" name="barcode" type="number"
+                                        placeholder="Digite el código de barra" value="{{old('barcode')}}" min="1"/>
+                                    <label for="codigoBarra">Código de Barra</label>
                                 </div>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
                                 <div class="form-floating mb-3 mb-md-0">
-                                    <input class="form-control" id="fechaVencimiento" type="date"
-                                        placeholder="Digite la fecha de vencimiento" />
-                                    <label for="fechaVencimiento">Fecha de Vencimiento</label>
+                                    <input class="form-control" id="quantity" name="quantity"type="number"
+                                        placeholder="Digite la fecha de vencimiento" value="{{old('quantity')}}" min="1"/>
+                                    <label for="cantidad">Cantidad</label>
                                 </div>
                             </div>
                             <div class="col-md-6">
                                 <div class="form-floating mb-3 mb-md-0">
-                                    <input class="form-control" id="precio" type="number"
-                                        placeholder="Digite el precio del producto" />
+                                    <input class="form-control" id="price" name="price" type="number"
+                                        placeholder="Digite el precio del producto" value="{{old('price')}}" min="1"/>
                                     <label for="precio">Precio</label>
                                 </div>
+                            </div>
+                        </div>
+                        <div class="mt-4 mb-0">
+                            <div class="d-grid">
+                                <label for="status">Estatus</label>
+                                <select name="status" class="form-control  id="status">
+                                    <option value="1" {{ old('status') === 1 ? 'selected' : '' }}>Activo</option>
+                                    <option value="0" {{ old('status') === 0 ? 'selected' : '' }}>Inactivo</option>
+                                </select>
                             </div>
                         </div>
                         <div class="mt-4 mb-0">
@@ -64,6 +74,15 @@
                     </form>
                 </div>
             </div>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
         </div>
     </div>
 </main>
