3bfa124aa814641d7948aa24ff454d667f0fc46a
Huptam <moisespayares6@gmail.com>
2022-08-09 18:34:11 -0500

    update tags: links and scripts

--- resources/views/layout/baseDashboard.blade.php
+++ resources/views/layout/baseDashboard.blade.php
@@ -9,7 +9,7 @@
     <meta name="author" content="Moisés" />
     <title>DistroApp - @yield('titulo')</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
+    <link rel="stylesheet" href="{{URL::asset('css/styles.css')}}">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 
@@ -50,14 +50,13 @@
             @yield('main')
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
-    </script>
-    <script src="js/scripts.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="{{URL::asset('js/scripts.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-    <script src="assets/demo/chart-area-demo.js"></script>
-    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="{{URL::asset('assets/demo/chart-area-demo.js')}}"></script>
+    <script src="{{URL::asset('assets/demo/chart-bar-demo.js')}}"></script>
     <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
-    <script src="js/datatables-simple-demo.js"></script>
+    <script src="{{URL::asset('js/datatables-simple-demo.js')}}"></script>
 </body>
 
 </html>
